bc326d35d0f1b63dd3e71d2f05e1505841219d24
ntekle99 <113952568+ntekle99@users.noreply.github.com>
2024-04-26 23:02:17 -0700

    Create stick_figure.v

---
+++ stick_figure.v
@@ -0,0 +1,78 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    12:18:00 12/14/2017 
+// Design Name: 
+// Module Name:    vga_top 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+// Date: 04/04/2020
+// Author: Yue (Julien) Niu
+// Description: Port from NEXYS3 to NEXYS4
+//////////////////////////////////////////////////////////////////////////////////
+module vga_top(
+	input ClkPort,
+	input BtnC,
+	input BtnU, BtnR, BtnL, BtnD,
+	
+	//VGA signal
+	output hSync, vSync,
+	output [3:0] vgaR, vgaG, vgaB,
+	
+	//SSG signal 
+	output An0, An1, An2, An3, An4, An5, An6, An7,
+	output Ca, Cb, Cc, Cd, Ce, Cf, Cg, Dp,
+	
+	//output MemOE, MemWR, RamCS, 
+	output QuadSpiFlashCS
+	);
+	
+	wire Reset;
+	assign Reset=BtnC;
+	
+	wire bright;
+	wire[9:0] hc, vc;
+	wire[15:0] score;
+	wire [6:0] ssdOut;
+	wire [3:0] anode;
+	wire [11:0] rgb;
+	
+	reg [27:0]	DIV_CLK;
+	always @ (posedge ClkPort, posedge Reset)  
+	begin : CLOCK_DIVIDER
+      if (Reset)
+			DIV_CLK <= 0;
+	  else
+			DIV_CLK <= DIV_CLK + 1'b1;
+	end
+	wire move_clk;
+	assign move_clk=DIV_CLK[19]; //slower clock to drive the movement of objects on the vga screen
+	
+	
+	display_controller dc(.clk(ClkPort), .hSync(hSync), .vSync(vSync), .bright(bright), .hCount(hc), .vCount(vc));
+	vga_bitchange vbc(.clk(move_clk), .reset(BtnC), .bright(bright), .hCount(hc), .vCount(vc), .rgb(rgb), .score(score), .up(BtnU), .down(BtnD),.left(BtnL),.right(BtnR));
+
+	assign {Ca, Cb, Cc, Cd, Ce, Cf, Cg} = ssdOut[6 : 0];
+    assign {An7, An6, An5, An4, An3, An2, An1, An0} = {4'b1111, anode};
+
+	
+	assign vgaR = rgb[11 : 8];
+	assign vgaG = rgb[7  : 4];
+	assign vgaB = rgb[3  : 0];
+	
+	// disable mamory ports
+	//assign {MemOE, MemWR, RamCS, QuadSpiFlashCS} = 4'b1111;
+	assign QuadSpiFlashCS = 1'b1;
+
+endmodule
